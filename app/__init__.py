1b432415299885b5fffa3d00796230a84d1522be
Okan KURTAR <dorokozmer@gmail.com>
2025-06-09 21:06:22 +0300

    update

--- app/__init__.py
+++ app/__init__.py
@@ -72,11 +72,15 @@ def create_app():
     # Google'ı bir OAuth sağlayıcısı olarak kaydet
     oauth.register(
         name='google',
+        client_id=app.config.get("GOOGLE_CLIENT_ID"),
+        client_secret=app.config.get("GOOGLE_CLIENT_SECRET"),
         server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
         client_kwargs={
             'scope': 'openid email profile'
         }
     )
+
+
 
     # ✨ Jinja filtresini kaydet
     app.jinja_env.filters['to_alphanumeric_bed_id'] = to_alphanumeric_bed_id
