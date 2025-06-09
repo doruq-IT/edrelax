f3eb7a9fb1e7b85d0adb588c09d0b31268e64d2c
Okan KURTAR <dorokozmer@gmail.com>
2025-06-09 05:53:59 +0300

    update

--- run.py
+++ run.py
@@ -1,10 +1,19 @@
+# from app import create_app
+# from app.extensions import socketio
+# from app import socket_events  # Bu satırı run.py dosyanda tut
+
+# app = create_app()
+
+# if __name__ == '__main__':
+#     socketio.run(app, debug=False, host='0.0.0.0', port=8000, use_reloader=False)
+
+
+# run.py
 from app import create_app
 from app.extensions import socketio
-from app import socket_events  # Bu satırı run.py dosyanda tut
+from app import socket_events  # unutma
 
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, debug=False, host='0.0.0.0', port=8000, use_reloader=False)
-
-
+    socketio.run(app, host='0.0.0.0', port=8000)
