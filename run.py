275712e11a4847130eb8caa29b09dfb8fcf459fa
Okan KURTAR <dorokozmer@gmail.com>
2025-06-11 20:35:57 +0300

    update monkey

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
-# run.py
+from gevent import monkey
+monkey.patch_all()
+# ---------------------
+
 from app import create_app
 from app.extensions import socketio
 from app import socket_events  # unutma
@@ -6,4 +9,6 @@ from app import socket_events  # unutma
 app = create_app()
 
 if __name__ == '__main__':
+    # Bu blok, doğrudan 'python run.py' komutuyla çalıştırıldığında kullanılır.
+    # Gunicorn ile çalışırken bu kısım Gunicorn tarafından yönetilir.
     socketio.run(app, host='0.0.0.0', port=8000)
