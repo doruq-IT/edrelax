de1a367f4ec10013b1dd2e28b5ef79f3d0ddfa8a
Okan KURTAR <dorokozmer@gmail.com>
2025-06-14 16:25:05 +0300

    update

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
-# from gevent import monkey
-# monkey.patch_all()
-# # ---------------------
+from gevent import monkey
+monkey.patch_all()
 
 from app import create_app
 from app.extensions import socketio
